f8af5ffd74128ebbf9ef23b91ae383ead3f80ec3
Jonah Meijers <jonah@Jonahs-MacBook-Pro.local>
2017-04-26 17:29:51 +0200

    werkt! (deels)

--- jonahjs.js
+++ jonahjs.js
@@ -1,8 +1,14 @@
+var elements = document.body.getElementsByTagName("*");
 window.onload = function() {
-    var elements = document.body.getElementsByTagName("*");
-    for (var i = 0; i < document.body.children.length / (Math.floor(Math.random() * 3 + 1)); i++) {
-        var image = document.createElement("IMG");
-        image.src = "http://www.jonahjs.nl/pictures/" + (Math.floor(Math.random() * 4)) + ".jpg";
-        document.body.insertBefore(image, elements[Math.floor(Math.random() * elements.length)]);
+    for (var i = 0; i < elements.length / (Math.floor(Math.random() * 3 + 1)); i++) {
+        setTimeout(function() {
+            var selectedElement = elements[Math.floor(Math.random() * elements.length)];
+            console.log(selectedElement);
+            var image = document.createElement("IMG");
+            image.src = "http://www.jonahjs.nl/pictures/" + (Math.floor(Math.random() * 4 + 1)) + ".jpg";
+            image.style.width = "200px";
+            image.style.height = "200px";
+            selectedElement.parentNode.insertBefore(image, selectedElement);
+        }, 900*i);
     }
 };
