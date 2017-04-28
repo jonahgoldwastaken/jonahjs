18a69038005543f9d3ec2b0dcf9518ddc0f9b9ff
Jonah Meijers <jonah@Jonahs-MacBook-Pro.local>
2017-04-28 15:07:21 +0200

    v1.6

--- jonahjs.js
+++ jonahjs.js
@@ -1,7 +1,24 @@
 var elements = document.body.getElementsByTagName("*");
 var textElements = document.body.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a, li");
 var jonah = "Jonah ";
-var t = 0;
+var t = -1;
+
+function addJonah(currentElement) {
+    currentElement.innerHTML += jonah;
+}
+
+function injectSauce() {
+    t++;
+    var amntOfWords = textElements[t].innerHTML.split(' ').length;
+    textElements[t].innerHTML = "";
+    var ding = textElements[t];
+    for (var o = 0; o < amntOfWords; o++) {
+        setTimeout(function() {
+            addJonah(ding);
+        }, 25 * o);
+    }
+}
+
 window.onload = function() {
     for (var i = 0; i < elements.length / (Math.floor(Math.random() * 3 + 1)); i++) {
         setTimeout(function() {
@@ -11,18 +28,9 @@ window.onload = function() {
             image.style.width = "200px";
             image.style.height = "200px";
             selectedElement.parentNode.insertBefore(image, selectedElement);
-        }, 900 * i);
+        }, 200 * i);
     }
     for (i = 0; i < textElements.length; i++) {
-        setTimeout(injectSauce, 900 * i);
+        setTimeout(injectSauce, 100 * i);
     }
 };
-
-function injectSauce() {
-    var amntOfWords = textElements[t].innerHTML.split(' ').length;
-    textElements[t].innerHTML = "";
-    for (i = 0; i < amntOfWords; i++) {
-        textElements[t].innerHTML += jonah;
-    }
-    t++;
-}
