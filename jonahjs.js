8268f871d1866c2b6b8a9ad1e5147faae006ef8c
Jonah Meijers <jonahmeijers97@gmail.com>
2017-12-04 21:41:45 +0100

    made JonahJS actually work again

--- jonahjs.js
+++ jonahjs.js
@@ -2,29 +2,31 @@ var JonahJS = {
     text: "Jonah",
     image: "http://www.jonahjs.nl/pictures/jonah.jpg",
     init: function() {
-        var textElements = document.body.querySelectorAll("h1, h2, h3, h4, h5, h6, p, ul>li, span, a:not(:empty)");
+        var textElements = document.body.querySelectorAll("h1, h2, h3, h4, h5, h6, p, ul>li, span, a:not(:empty), legend");
         var imageElements = document.body.querySelectorAll("img");
         for (var i = 0; i < imageElements.length; i++) {
-            setTimeout(changeAllImagesToJonah(i), 1000 * i);
+            setTimeout(this.changeAllImagesToJonah(i), 1000 * i);
         }
         for (i = 0; i < textElements.length; i++) {
-            setTimeout(injectJonah(i), 100 * i);
+            setTimeout(this.injectJonah(i), 100 * i);
         }
     },
     changeTextToJonah: function(currentElement) {
         currentElement.innerHTML += JonahJS.text + " ";
     },
     changeAllImages: function(index) {
+        var imageElements = document.body.querySelectorAll("img");
         imageElements[index].src = JonahJS.image;
     },
     injectJonah: function(index) {
+        var textElements = document.body.querySelectorAll("h1, h2, h3, h4, h5, h6, p, ul>li, span, a:not(:empty), legend");
         var amntOfWords = textElements[index].innerHTML.split(' ').length;
         textElements[index].innerHTML = "";
         var currentTextElement = textElements[index];
         for (var i = 0; i < amntOfWords; i++) {
             setTimeout(function () {
-                changeTextToJonah(currentTextElement);
-            }, 25 * o);
+                JonahJS.changeTextToJonah(currentTextElement);
+            }, 25 * i);
         }
     }
 }
